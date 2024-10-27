6678d996c501a011780a964fd958f4b60c316cbd
biwott77 <biwotmoi@gmail.com>
2024-10-27 13:58:15 -0400

    Update with new changes

--- src/Frontend/Navbar.js
+++ src/Frontend/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 import moik from './moik.jpg';
@@ -6,25 +6,44 @@ import moik from './moik.jpg';
 import SignUp from './SignUp'; */
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-      <div className="navbar-left">
+      <div className="navbar-logo">
         <Link to="/">
           <img src={moik} alt="Moik" className="navbar-image" />
         </Link>
-        <ul className="navbar-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
       </div>
-      {/* <div className="navbar-right">
-        <Login />
-        <SignUp />
-      </div> */}
+
+      {/* Toggle Icon */}
+      <div 
+        className={`toggle-icon ${isOpen ? 'active' : ''}`} 
+        onClick={toggleMenu}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul className={`navbar-links ${isOpen ? 'active' : ''}`}>
+        <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
+        <li><Link to="/about" onClick={() => setIsOpen(false)}>About</Link></li>
+        <li><Link to="/projects" onClick={() => setIsOpen(false)}>Projects</Link></li>
+        <li><Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link></li>
+      </ul>
     </nav>
   );
 };
 
 export default Navbar;
+
+
+
+
+      
+

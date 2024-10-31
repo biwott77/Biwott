1a389d08d9ff0da9cee52e77c0330d05787fbcc6
biwott77 <biwotmoi@gmail.com>
2024-10-31 03:44:23 -0400

    Initial commit

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'development' 
-    ? 'http://localhost:8080'
-    : 'https://api-8maj9wzty-mois-projects-2e2312ad.vercel.app'; 
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
